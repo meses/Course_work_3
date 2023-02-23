edceb8c523752f5ae29e0e00f1e68961e7b4ce61
Artem <akirillov@itps-russia.ru>
2023-02-23 19:36:04 +0500

    Написание кода основной программы

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-from utils import read_file
+from utils import read_file, execute_transactios, hide_symbols_account, transfom_date_fomat
 
-transactions = []
-transactions = read_file()
+transaction_list = read_file()
+filtered_sorted_list = execute_transactios(transaction_list, 'date', 'EXECUTED')[:5]
 
 
-print(type(transactions))
+for i in filtered_sorted_list:
+    if 'from' in i.keys():
+        print(transfom_date_fomat(i['date']), i['description'])
+        print(f"{hide_symbols_account(i['from'])} -> {hide_symbols_account(i['to'])}")
+        print(i['operationAmount']['amount'], i['operationAmount']['currency']['name'])
+        print('')
+
+    else:
+        print(transfom_date_fomat(i['date']), i['description'])
+        print(hide_symbols_account(i['to']))
+        print(i['operationAmount']['amount'], i['operationAmount']['currency']['name'])
+        print('')
+
+
